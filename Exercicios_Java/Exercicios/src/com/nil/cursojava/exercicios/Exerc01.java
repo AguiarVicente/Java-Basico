224f2c9343cf4a537b0fb5bfb8d15fe245d94689
Aguiar <nil@Aguiar.local>
2021-01-02 22:09:25 -0300

    Exercicios em Java

---
+++ Exercicios_Java/Exercicios/src/com/nil/cursojava/exercicios/Exerc01.java
@@ -0,0 +1,16 @@
+package com.nil.cursojava.exercicios;
+
+public class Exerc01 {
+
+	public static void main(String[] args) {
+		
+		/*	Crie um programa 
+		 *  Que diga Olá Mundo (Hello, World!)
+		 * */
+		
+		System.out.println("Hello, World!");
+		System.out.println("Olá Mundo!");
+
+	}
+
+}
